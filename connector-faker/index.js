b5afca7439792ab497ac115a424da84b54daeeb4
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-07-05 22:51:07 -0500

    Fix connector-faker bugs

--- connector-faker/index.js
+++ connector-faker/index.js
@@ -1,6 +1,8 @@
-exports.faker = function faker({ FAKER_LOCALE = 'en', FAKER_SEED = null }) {
+function faker({ FAKER_LOCALE = 'en', FAKER_SEED = null }) {
   const fake = require('faker');
-  fake.locale(FAKER_LOCALE);
+  fake.locale =FAKER_LOCALE;
   if (FAKER_SEED) fake.seed(FAKER_SEED);
   return fake;
 };
+
+module.exports = faker
