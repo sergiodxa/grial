fad70c35d155275c8287895413d871a36332ba0a
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-07-11 20:07:56 -0500

    Simplify resolvers and schema imports

--- server/api/resolvers.js
+++ server/api/resolvers.js
@@ -1,11 +1,9 @@
-const fs = require('fs');
 const { resolve } = require('path');
-const { promisify } = require('util');
-
-const stat = promisify(fs.stat);
 
 module.exports = async function getResolvers(BASE_PATH) {
-  const path = resolve(`${BASE_PATH}/resolvers.js`);
-  if (await stat(path)) return require(path);
-  throw new ReferenceError('The file `./resolvers.js` is required.');
+  try {
+    return require(resolve(`${BASE_PATH}/resolvers.js`));
+  } catch (error) {
+    throw new ReferenceError('The file `./resolvers.js` is required.');
+  }
 };
