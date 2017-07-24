7d2115118dc5253caaadedcd74c621aeb8611480
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-07-24 13:52:22 -0500

    Merge instances into a single map

---
+++ server/utils/merge-instances.js
@@ -0,0 +1,12 @@
+/**
+ * Merge instances into a single map
+ * @param  {Object} instances The current instances
+ * @param  {String} name      The instance name
+ * @param  {Object} instance  The instance to merge
+ * @return {Object}           The new instances map
+ */
+function mergeInstances(instances, [name, instance]) {
+  return Object.assign({}, instances, { [name]: instance });
+}
+
+module.exports = mergeInstances;
