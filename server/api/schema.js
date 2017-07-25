8ec540eb3f560c01ecdb3b9ccba257276fbf084b
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-07-25 04:09:59 -0500

    Add support for schema.graphql file

--- server/api/schema.js
+++ server/api/schema.js
@@ -1,15 +1,27 @@
+// native
 const fs = require('fs')
 const { resolve } = require('path')
 const { promisify } = require('util')
 
+// packages
+const fileExists = require('file-exists')
+
+// promisified
 const readFile = promisify(fs.readFile)
+const exists = promisify(fileExists)
 
 module.exports = async function getSchema(BASE_PATH) {
   try {
-    return readFile(resolve(`${BASE_PATH}/schema.gql`), 'utf8')
+    if (await exists(resolve(`${BASE_PATH}/schema.gql`))) {
+      return readFile(resolve(`${BASE_PATH}/schema.gql`), 'utf8')
+    }
+    if (await exists(resolve(`${BASE_PATH}/schema.graphql`))) {
+      return readFile(resolve(`${BASE_PATH}/schema.graphql`), 'utf8')
+    }
+    throw new ReferenceError('The file `./schema.gql` or `./schema.graphql` is required.')
   } catch (error) {
-    if (error.code === 'MODULE_NOT_FOUND') {
-      throw new ReferenceError('The file `./schema.gql` is required.')
+    if (error.code === 'ENOENT') {
+      throw new ReferenceError('The file `./schema.gql` or `./schema.graphql` is required.')
     }
     throw error
   }
