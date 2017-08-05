b61779b6f5ee020bcdbb5312c4750aa6c24fde28
Emilien GUILMINEAU <guilmineau.emilien@gmail.com>
2017-08-05 23:17:18 +0200

    Allow GraphQL schema splitting

--- server/api/schema.js
+++ server/api/schema.js
@@ -1,23 +1,42 @@
 // native
+const os = require('os')
 const fs = require('fs')
 const { resolve } = require('path')
 const { promisify } = require('util')
 
-// packages
-const fileExists = require('file-exists')
-
 // promisified
 const readFile = promisify(fs.readFile)
-const exists = promisify(fileExists)
+const readdir = promisify(fs.readdir)
+const stat = promisify(fs.stat)
+
+// Returns only graphql files from a folder
+const getSchemaFiles = async function (folder) {
+  const files = (await readdir(resolve(folder))) || []
+
+  return files
+    .map(file => folder + '/' + file)
+    .filter(async file => (await stat(file)).isFile())
+    .filter(file => file.endsWith('.gql') || file.endsWith('.graphql'))
+}
+// Returns concatenated schemas content
+const getSchemasContent = async function(folder) {
+  const files = await getSchemaFiles(resolve(folder))
+  const schemas = await Promise.all(files.map(async (file) => (await readFile(file, 'utf8'))))
+  return (schemas.length > 0) && schemas.reduce((previous, current) => previous + os.EOL + current)
+}
 
 module.exports = async function getSchema(BASE_PATH) {
   try {
-    if (await exists(resolve(`${BASE_PATH}/schema.gql`))) {
-      return readFile(resolve(`${BASE_PATH}/schema.gql`), 'utf8')
+    let schema = await getSchemasContent(resolve(`${BASE_PATH}`))
+    if (schema && schema.length > 0) {
+      return schema
     }
-    if (await exists(resolve(`${BASE_PATH}/schema.graphql`))) {
-      return readFile(resolve(`${BASE_PATH}/schema.graphql`), 'utf8')
+
+    schema = await getSchemasContent(resolve(`${BASE_PATH}/schemas/`))
+    if (schema && schema.length > 0) {
+      return schema
     }
+    
     throw new ReferenceError('The file `./schema.gql` or `./schema.graphql` is required.')
   } catch (error) {
     if (error.code === 'ENOENT') {
