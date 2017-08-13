5bb337776d2d51fc9a2df03c6d11329a3e91d4f2
antoniosilva <antoniopresto@gmail.com>
2017-08-13 00:12:28 -0300

    add cache for connector-mongodb connections [#12]

--- connector-mongodb/index.js
+++ connector-mongodb/index.js
@@ -1,5 +1,7 @@
 const MongoClient = require('mongodb')
 
+const connectionsCache = {}
+
 async function mongodb({
   MONGO_USER,
   MONGO_PASS,
@@ -7,14 +9,24 @@ async function mongodb({
   MONGO_NAME,
   MONGO_URL = null
 }) {
-  const URL = MONGO_URL || `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}/${MONGO_NAME}`
+  const URL =
+    MONGO_URL ||
+    `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}/${MONGO_NAME}`
 
-  return new Promise((resolve, reject) => {
-    MongoClient.connect(URL, (error, connection) => {
-      if (error) return reject(error)
-      resolve(connection)
+  if (!connectionsCache[URL]) {
+    connectionsCache[URL] = new Promise(function(resolve, reject) {
+      MongoClient.connect(URL, (error, connection) => {
+        if (error) {
+          console.log(error)
+          reject(error)
+        }
+
+        resolve(connection)
+      })
     })
-  })
+  }
+
+  return connectionsCache[URL]
 }
 
 module.exports = mongodb
