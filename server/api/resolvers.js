6e54090a6ba6f748ad70bdf2dc71e3d0e5dd457d
Jörn Röder <kontakt@joernroeder.de>
2017-08-27 03:42:46 +0200

    removed file extensions to allow node module style imports. @see https://nodejs.org/api/modules.html#modules_all_together

--- server/api/resolvers.js
+++ server/api/resolvers.js
@@ -2,10 +2,10 @@ const { resolve } = require('path')
 
 module.exports = async function getResolvers(BASE_PATH) {
   try {
-    return require(resolve(`${BASE_PATH}/resolvers.js`))
+    return require(resolve(`${BASE_PATH}/resolvers`))
   } catch (error) {
     if (error.code === 'MODULE_NOT_FOUND') {
-      throw new ReferenceError('The file `./resolvers.js` is required.')
+      throw new ReferenceError('The file `./resolvers.js` or `./resolvers/index.js` is required.')
     }
     throw error
   }
