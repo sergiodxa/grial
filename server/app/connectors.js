7e5ec75b7aac00c030be272a56f7c3bf6efc214e
Jörn Röder <joernroeder@users.noreply.github.com>
2017-08-28 20:32:09 +0200

    removed file extensions to allow node module style imports (#18)
    * removed file extensions to allow node module style imports. @see https://nodejs.org/api/modules.html#modules_all_together
    
    * removed file extension in /app files

--- server/app/connectors.js
+++ server/app/connectors.js
@@ -2,7 +2,7 @@ const { resolve } = require('path')
 
 module.exports = async function getConnectors(BASE_PATH) {
   try {
-    return require(resolve(`${BASE_PATH}/connectors.js`))
+    return require(resolve(`${BASE_PATH}/connectors`))
   } catch (error) {
     if (error.code === 'MODULE_NOT_FOUND') {
       return {}
--- server/app/models.js
+++ server/app/models.js
@@ -2,7 +2,7 @@ const { resolve } = require('path')
 
 module.exports = async function getModels(BASE_PATH) {
   try {
-    return require(resolve(`${BASE_PATH}/models.js`))
+    return require(resolve(`${BASE_PATH}/models`))
   } catch (error) {
     if (error.code === 'MODULE_NOT_FOUND') {
       return {}
